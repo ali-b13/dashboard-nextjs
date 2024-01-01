c62f4af225eb2ffdf3c24f5e7ef9ee9fe2a75d99
ali-b13 <ali322213a@gmail.com>
2024-01-01 18:27:31 +0300

    uploading the files first time

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter,Alexandria ,Lusitana} from "next/font/google"
+
+export const inter =Inter({subsets:["latin"]})
+
+export const alexandria = Alexandria({weight:"600",subsets:["latin-ext"]})
+
+export const lusitana = Lusitana({ weight: ["400","700"], subsets: ["latin"] })
